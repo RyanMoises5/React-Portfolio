f5cf32d7b9eed68261e4f1fc607b703fcad8a2e5
Ryan M <ryan.a.moises@gmail.com>
2024-05-16 17:05:01 -0400

    Added starter file/folder structure; main.jsx - added routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import ErrorPage from './pages/ErrorPage.jsx'
+import HomePage from './pages/HomePage.jsx'
+import TestPage from './pages/TestPage.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />
+      },
+      {
+        path: 'test',
+        element: <TestPage />
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router} />
 )
